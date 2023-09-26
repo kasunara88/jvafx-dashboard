4dcb7b89335feb65baad73cc8b73364cd4193edf
kasunara88 <wmaravinda@gmail.com>
2023-09-26 19:00:02 +0530

    dashboard UI completed

---
+++ src/main/java/lk/ijse/stock/management/Launcher.java
@@ -0,0 +1,25 @@
+package lk.ijse.stock.management;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/dashboard_form.fxml"));
+        Scene scene = new Scene(rootNode);
+
+        stage.setScene(scene);
+        stage.setTitle("Dashboard");
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
---
+++ src/main/java/lk/ijse/stock/management/LauncherWrapper.java
@@ -0,0 +1,8 @@
+package lk.ijse.stock.management;
+
+public class LauncherWrapper {
+    public static void main(String[] args){
+        Launcher.main(args);
+    }
+
+}
